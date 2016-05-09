f9f5b90a53892e595f7b1e23129721f1dca4e75f
huai-shu <cpfordream@163.com>
2016-05-09 18:20:37 +0800

    noting

--- gen/R.java.d
+++
@@ -1,7 +0,0 @@
-D:\Android\KKHAndroidUITest\gen\com\doctor\test\R.java \
- : D:\Android\KKHAndroidUITest\res\drawable-hdpi\ic_launcher.png \
-D:\Android\KKHAndroidUITest\res\drawable-ldpi\ic_launcher.png \
-D:\Android\KKHAndroidUITest\res\drawable-mdpi\ic_launcher.png \
-D:\Android\KKHAndroidUITest\res\drawable-xhdpi\ic_launcher.png \
-D:\Android\KKHAndroidUITest\res\values\strings.xml \
-D:\Android\KKHAndroidUITest\bin\AndroidManifest.xml \
